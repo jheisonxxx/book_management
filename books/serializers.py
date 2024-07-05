b1a2ea5673ee19c8d3f5aa71ff8419180124b6d6
jheisonxxx <jheison.talavera@gmail.com>
2024-07-05 11:27:54 -0500

    Custom serializers.py

--- books/serializers.py
+++ books/serializers.py
@@ -1,15 +1,16 @@
 
 from rest_framework import serializers
+from datetime import datetime
 from .models import Book
 
 class BookSerializer(serializers.ModelSerializer):
     id = serializers.CharField(source='_id', read_only=True)
-    #published_year = serializers.SerializerMethodField()
+    published_year = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Book
-        fields = ('id', 'title', 'author', 'published_date', 'genre', 'price')
-        #Agregar published year
+        fields = ('id', 'title', 'author', 'published_date','published_year', 'genre', 'price')
 
-    #def get_published_year(self, obj):
-    #    return obj.published_date.year
+    def get_published_year(self, obj):
+        date = datetime.strptime(str(obj['published_date']),"%Y-%m-%d")
+        return str(date.year)
