3c9482b0ed460e278ec3508d9b4f257c23a22da9
jheisonxxx <jheison.talavera@gmail.com>
2024-07-05 09:40:27 -0500

    Update views.py

--- books/views.py
+++ books/views.py
@@ -1,18 +1,25 @@
-
 from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
 from bson import ObjectId
 from .config_mongo import db
 import logging
 
 class BookViewSet(viewsets.ViewSet):
     def list(self, request):
-        books = db.books.find()
-        serializer = BookSerializer(books, many=True)
-        return Response(serializer.data)
+        try:
+            paginator = PageNumberPagination()
+            books = list(db.books.find())
+            for book in books:
+                book['_id'] = str(book['_id'])  # Convert ObjectId to string
+            result_page = paginator.paginate_queryset(books, request)
+            serializer = BookSerializer(result_page, many=True)
+            return paginator.get_paginated_response(serializer.data)
+        except Exception as e:
+            return Response({"error": f"Error fetching book list: {str(e)}"}, status=500)
 
     def create(self, request):
         serializer = BookSerializer(data=request.data)
@@ -47,5 +54,3 @@ def average_price_by_year(request, year):
     result = list(collection.aggregate(pipeline))
     average_price = result[0]['average_price'] if result else 0
     return Response({'year': year, 'average_price': average_price})
-
-
